424c9b8ca0ef1cde15c8c7ef6da14f1670322be5
loesxje <loesmodderman@gmail.com>
2018-02-23 12:59:26 +0100

    succesful print data

--- Experiment_preprocessing.py
+++ Experiment_preprocessing.py
@@ -1,22 +1,42 @@
 #-*- coding: utf-8 -*-
 import wave
-import struct
-# READ .WAV FILE
-# =============================================================================
-# w = wave.open('C:/Users/Loes/Documents/GitHub/Projectstage/audiovoorbeelden/074_heartbeat-noise-machine.wav', 'r')
-# for i in range(w.getnframes()):
-#     print i
-#     frame = w.readframes(i)
-# =============================================================================
-aud = open("C:/Users/Loes/Documents/GitHub/Projectstage/audiovoorbeelden/074_heartbeat-noise-machine.wav", "r")
+import numpy as np
 
-data = []
+def _wav2array(nchannels, sampwidth, data):
+    """data must be the string containing the bytes from the wav file."""
+    num_samples, remainder = divmod(len(data), sampwidth * nchannels)
+    if remainder > 0:
+        raise ValueError('The length of data is not a multiple of '
+                         'sampwidth * num_channels.')
+    if sampwidth > 4:
+        raise ValueError("sampwidth must not be greater than 4.")
 
-for i in range(len(aud.read())):
-    SnString = aud.read(2)
-    Sn = struct.unpack("h", SnString)
-    data.append(Sn)
-print data    
+    if sampwidth == 3:
+        a = np.empty((num_samples, nchannels, 4), dtype=np.uint8)
+        raw_bytes = np.fromstring(data, dtype=np.uint8)
+        a[:, :, :sampwidth] = raw_bytes.reshape(-1, nchannels, sampwidth)
+        a[:, :, sampwidth:] = (a[:, :, sampwidth - 1:sampwidth] >> 7) * 255
+        result = a.view('<i4').reshape(a.shape[:-1])
+    else:
+        # 8 bit samples are stored as unsigned ints; others as signed ints.
+        dt_char = 'u' if sampwidth == 1 else 'i'
+        a = np.fromstring(data, dtype='<%s%d' % (dt_char, sampwidth))
+        result = a.reshape(-1, nchannels)
+    return result
+
+ # READ .WAV FILE
+w = wave.open('C:/Users/Loes/Documents/GitHub/Projectstage/audiovoorbeelden/074_heartbeat-noise-machine.wav', 'r')
+rate = w.getframerate()
+nchannels = w.getnchannels()
+sampwidth = w.getsampwidth()
+data = w.readframes(w.getnframes())
+w.close()
+array = _wav2array(nchannels, sampwidth, data)
+print(array)
+ 
+ 
+
+
 
 
 # VECTORIZE DATA
