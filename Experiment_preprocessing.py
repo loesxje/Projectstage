a9dd11eaaccdd0c3afe690b2cfdc142f4556b08a
loesxje <loesmodderman@gmail.com>
2018-02-23 15:32:17 +0100

    add split vectors

--- Experiment_preprocessing.py
+++ Experiment_preprocessing.py
@@ -1,50 +1,58 @@
 #-*- coding: utf-8 -*-
-import wave
+from scipy.io import wavfile
+from matplotlib import pyplot as plt
 import numpy as np
+import math
 
-def _wav2array(nchannels, sampwidth, data):
-    """data must be the string containing the bytes from the wav file."""
-    num_samples, remainder = divmod(len(data), sampwidth * nchannels)
-    if remainder > 0:
-        raise ValueError('The length of data is not a multiple of '
-                         'sampwidth * num_channels.')
-    if sampwidth > 4:
-        raise ValueError("sampwidth must not be greater than 4.")
 
-    if sampwidth == 3:
-        a = np.empty((num_samples, nchannels, 4), dtype=np.uint8)
-        raw_bytes = np.fromstring(data, dtype=np.uint8)
-        a[:, :, :sampwidth] = raw_bytes.reshape(-1, nchannels, sampwidth)
-        a[:, :, sampwidth:] = (a[:, :, sampwidth - 1:sampwidth] >> 7) * 255
-        result = a.view('<i4').reshape(a.shape[:-1])
-    else:
-        # 8 bit samples are stored as unsigned ints; others as signed ints.
-        dt_char = 'u' if sampwidth == 1 else 'i'
-        a = np.fromstring(data, dtype='<%s%d' % (dt_char, sampwidth))
-        result = a.reshape(-1, nchannels)
-    return result
+# Load the data and calculate the time of each sample
+samplerate, data = wavfile.read('C:/Users/Loes/Documents/GitHub/Projectstage/audiovoorbeelden/heartbeat-noise-machine.wav')
 
- # READ .WAV FILE
-w = wave.open('C:/Users/Loes/Documents/GitHub/Projectstage/audiovoorbeelden/074_heartbeat-noise-machine.wav', 'r')
-rate = w.getframerate()
-nchannels = w.getnchannels()
-sampwidth = w.getsampwidth()
-data = w.readframes(w.getnframes())
-w.close()
-array = _wav2array(nchannels, sampwidth, data)
-print(array)
- 
- 
+# Length of sampling Parameter
+p = len(data)
+
+times = np.arange(len(data[:p]))/float(samplerate)
 
 
 
 
 # VECTORIZE DATA
+# Stereo to Mono
+def stereo_to_mono(audio_data):
+    mono = []
+    for i in range(len(audio_data[:p])):
+        mono.append((audio_data[i,0] + data[i,1])/2);
+        
+    return mono
 
+# =============================================================================
+# # Make the plot
+# # You can tweak the figsize (width, height) in inches
+# plt.figure(figsize=(30, 4))
+# #plt.fill_between(times, data[:p,0], data[:p,1], color='k') 
+# plt.plot(times, mono[:p])
+# plt.xlim(times[0], times[-1])
+# plt.xlabel('time (s)')
+# plt.ylabel('amplitude')
+# # You can set the format by changing the extension
+# # like .pdf, .svg, .eps
+# plt.show()
+# =============================================================================
 
 
 # SPLIT VECTOR
-
+def split_vector(time, mono_audio):
+    interval_length = 0.02;
+    n_splits = int(math.ceil(time[-1]/interval_length));
+    interval_size = int(math.floor(len(mono_audio) / n_splits));
+    
+    intervals = []
+    n = 0
+    for i in range(n_splits+1):
+        intervals.append(mono_audio[0+n:interval_size+n]);
+        n += interval_size;
+    
+    return intervals
 
 
 # FOURIER TRANSFORM DATA
