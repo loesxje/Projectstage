b1537ea62cad864b22590ffc9d60af257ab2d714
loesxje <loesmodderman@gmail.com>
2018-02-23 11:10:51 +0100

    add read data

--- Experiment_preprocessing.py
+++ Experiment_preprocessing.py
@@ -1,11 +1,22 @@
-#Hellooooooo -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 import wave
-
+import struct
 # READ .WAV FILE
-w = wave.open('C:/Users/Loes/Dropbox/Projectstage_Loes&Ngoc/audiovoorbeelden', 'r')
-for i in range(w.getnframes()):
-    frame = w.readframes(i)
-    print frame
+# =============================================================================
+# w = wave.open('C:/Users/Loes/Documents/GitHub/Projectstage/audiovoorbeelden/074_heartbeat-noise-machine.wav', 'r')
+# for i in range(w.getnframes()):
+#     print i
+#     frame = w.readframes(i)
+# =============================================================================
+aud = open("C:/Users/Loes/Documents/GitHub/Projectstage/audiovoorbeelden/074_heartbeat-noise-machine.wav", "r")
+
+data = []
+
+for i in range(len(aud.read())):
+    SnString = aud.read(2)
+    Sn = struct.unpack("h", SnString)
+    data.append(Sn)
+print data    
 
 
 # VECTORIZE DATA
