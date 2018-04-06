d871a185e35589cff591aa6c96fb86335c985f4e
ngoccie <36759514+ngoccie@users.noreply.github.com>
2018-04-06 15:06:58 +0200

    changes

--- Convert .raw to .wav/temp_loop_over_files.m
+++
@@ -1,7 +0,0 @@
-clear, close, clc
-path = 'C:\Users\Loes\Documents\GitHub\Projectstage\Convert .raw to .wav\';
-k = 7;
-for i = 0:k
-    variable = sprintf('mic_44100_s16le_channel_%d_WAV.wav',i)
-    [x,Fs] = audioread(strcat(path,variable));
-end
