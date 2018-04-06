eeac584a1951044a435d8321abb04da43aef25d8
loesxje <loesmodderman@gmail.com>
2018-04-06 12:17:27 +0200

    loop over the files

--- Convert .raw to .wav/Converter_raw_to_wav.m
+++ Convert .raw to .wav/Converter_raw_to_wav.m
@@ -1,6 +1,15 @@
 % Multiple file raw audio file conversion
+clear, close, clc
+path = 'C:\Users\Loes\Documents\GitHub\Projectstage\Convert .raw to .wav\';
+k = 7;
+for i = 0:k
+    list(i) = sprintf('mic_44100_s16le_channel_%d_.raw',i)
+    [x,Fs] = audioread(strcat(path,variable));
+end
 
-list = dir ('mic_44100_s16le_channel_8.raw');
+
+
+%list = dir ('mic_44100_s16le_channel_8.raw');
 
 endianness='l';
 fs=44100;
--- Convert .raw to .wav/temp_loop_over_files.m
+++ Convert .raw to .wav/temp_loop_over_files.m
@@ -1,6 +1,7 @@
 clear, close, clc
 path = 'C:\Users\Loes\Documents\GitHub\Projectstage\Convert .raw to .wav\';
 k = 7;
-i = 0;
-variable = sprintf('mic_44100_s16le_channel_%d.wav',i)
-[x,Fs] = audioread(strcat(path,variable))
+for i = 0:k
+    variable = sprintf('mic_44100_s16le_channel_%d_WAV.wav',i)
+    [x,Fs] = audioread(strcat(path,variable));
+end
